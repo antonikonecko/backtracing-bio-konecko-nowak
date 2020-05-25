81ce930576dc9a0f49ac8f437a9e4e5847d75f22
antonikonecko <61695517+antonikonecko@users.noreply.github.com>
2020-05-25 13:19:39 +0200

    Update hierholzer.h

--- hierholzer.h
+++ hierholzer.h
@@ -1,8 +1,33 @@
 #ifndef __HIERHOLZER_H__
 #define __HIERHOLZER_H__
 
-void euler(struct graph *g, int *cycle) {
-    // TODO: implement this
-}
 
+void euler(struct graph *g, int *cycle)
+{
+    struct stack *s = stack_create(g->size * g->size);  
+    int u = 0;                                             
+    stack_push(s, u);                                   
+
+    while(!stack_is_empty(s))                           
+    {
+        u = stack_peek(s);
+        int v = 0;                              
+        while (v < g->size)                                
+        {
+            for(v = 0; v < g->size; v++)                   
+            {
+                if(graph_has_edge(g, u, v))                
+                {
+                    graph_remove_edge(g, u, v);           
+                    stack_push(s, v);                   
+                    u = v;                                 
+                    v = 0;                                 
+                    break;                                
+                }
+            }
+        }
+    *cycle = stack_pop(s);                              
+    cycle++;                                               
+    }
+}
 #endif
